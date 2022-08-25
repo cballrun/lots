0edcc2b01f840baa18b9c7493eefc0b56b418a0b
Carter Ball <carter.k.ball@gmail.com>
2022-08-24 22:48:02 -0400

    Add routes/view files for Card Index/Show, create CardsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/lots', to: 'lots#index'
   get 'lots/:id', to: 'lots#show'
+  get '/cards', to: 'cards#index'
+  get '/cards/:id', to: 'cards#show'
 end
