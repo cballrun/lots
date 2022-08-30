9c567d9bfb3b235435d98d0262f912087bdb35c7
Carter Ball <carter.k.ball@gmail.com>
2022-08-30 01:19:40 -0400

    Test for only displaying Holo cards

--- spec/features/cards/index_spec.rb
+++ spec/features/cards/index_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'the cards index page' do
   before :each do 
-    @lot_1 = Lot.create!(name: "Base Shadowless", total_cards: 102, original_150: true, release_year: 1999)
-    @lot_2 = Lot.create!(name: "Jungle", total_cards: 64, original_150: true, release_year: 1999)
-    @card_1 = @lot_1.cards.create!(name: "Magikarp", value: 3, holo: false, rarity_symbol: "Diamond", condition: "LP", english: true, first_edition: false)
-    @card_2 = @lot_2.cards.create!(name: "Kangaskhan", value: 35, holo: true, rarity_symbol: "Star", condition: "LP", english: true, first_edition: true)
+    @shadowless = Lot.create!(name: "Base Shadowless", total_cards: 102, original_150: true, release_year: 1999)
+    @jungle = Lot.create!(name: "Jungle", total_cards: 64, original_150: true, release_year: 1999)
+    @magikarp = @shadowless.cards.create!(name: "Magikarp", value: 3, holo: false, rarity_symbol: "Diamond", condition: "LP", english: true, first_edition: false)
+    @charizard = @shadowless.cards.create!(name: "Charizard", value: 500, holo: true, rarity_symbol: "Star", condition: "HP", english: true, first_edition: false)
+    @dragonair = @shadowless.cards.create!(name: "Dragonair", value: 150, holo: false, rarity_symbol: "Star", condition: "LP", english: true, first_edition: true)
+    @blastoise = @shadowless.cards.create(name: "Blastoise", value: 260, holo: true, rarity_symbol: "Star", condition: "LP", english: true, first_edition: false)
+    @kangaskhan = @jungle.cards.create!(name: "Kangaskhan", value: 35, holo: true, rarity_symbol: "Star", condition: "LP", english: true, first_edition: true)
     visit "/cards"
   end  
 
-  it 'displays the names of all cards' do
+  xit 'displays the names of all cards' do
     
-    expect(page).to have_content(@card_1.name)
-    expect(page).to have_content(@card_2.name)
+    expect(page).to have_content(@magikarp.name)
+    expect(page).to have_content(@kangaskhan.name)
   end
 
   it 'has a link to the card index page' do
@@ -25,6 +28,14 @@ RSpec.describe 'the cards index page' do
     click_on "All Lots"
 
     expect(current_path).to eq("/lots")
+  end
+
+  it 'only shows Cards if they are holo' do
+    expect(page).to have_content(@charizard.name)
+    expect(page).to have_content(@blastoise.name)
+
+    expect(page).to_not have_content(@magikarp.name)
+    expect(page).to_not have_content(@kangaskhan.name)
   end
 
 
